97feeae47cd6fee26f3126449959775dd151dfa0
Sebastian Vikstrom <sebastianvikstrom@Sebastians-MacBook-Pro.local>
2021-08-18 14:21:35 +0200

    added admin client

--- client/src/components/list/List.js
+++ client/src/components/list/List.js
@@ -6,7 +6,7 @@ import {
 import { useRef, useState } from "react";
 import ListItem from "../listitem/ListItem";
 
-function List() {
+function List({ list }) {
   const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
 
@@ -27,7 +27,7 @@ function List() {
 
   return (
     <div className="list">
-      <span className="listTitle">Continue to watch</span>
+      <span className="listTitle">{list.title}</span>
       <div className="wrapper">
         <ArrowBackIosOutlined
           className="sliderArrow left"
@@ -35,16 +35,9 @@ function List() {
           onClick={() => handleClick("left")}
         />
         <div className="container" ref={listRef}>
-          <ListItem index={0} />
-          <ListItem index={1} />
-          <ListItem index={2} />
-          <ListItem index={3} />
-          <ListItem index={4} />
-          <ListItem index={5} />
-          <ListItem index={6} />
-          <ListItem index={7} />
-          <ListItem index={8} />
-          <ListItem index={9} />
+          {list.content.map((item, i) => (
+            <ListItem index={i} item={item} key={i} />
+          ))}
         </div>
         <ArrowForwardIosOutlined
           className="sliderArrow right"
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,17 +1,42 @@
+import { useEffect, useState } from "react";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
+import axios from "axios";
 import "./home.scss";
 
-function Home() {
+function Home({ type }) {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMTNkYzgyZmRkYWQzMDgwOWI0ZmZkZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyOTE4NTc3NiwiZXhwIjoxNjI5NjE3Nzc2fQ.og9DXwnN7WnOL2UBhFuHiX9WyfINJ1N9A6UYLZBsLfk",
+            },
+          }
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
   return (
     <div className="home">
       <Navbar />
-      <Featured />
-      <List />
-      <List />
-      <List />
-      <List />
+      <Featured type={type} />
+      {lists.map((list) => (
+        <List list={list} key={list._id} />
+      ))}
     </div>
   );
 }
