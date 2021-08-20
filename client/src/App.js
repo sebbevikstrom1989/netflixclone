300d1ddfecfb84c797f4aadb3f43aaa016932652
sebbevikstrom1989 <sebastian@vikstroms.se>
2021-08-20 16:49:14 +0200

    Fixed some small things

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
     <Router>
       <Switch>
         <Route exact path="/">
-          {user ? <Home /> : <Redirect to="/register" />}
+          {user ? <Home /> : <Redirect to="/login" />}
         </Route>
         <Route path="/register">
           {!user ? <Register /> : <Redirect to="/" />}
--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { login } from "../../authContext/apiCalls";
 import { AuthContext } from "../../authContext/AuthContext";
 import "./login.scss";
@@ -40,7 +41,10 @@ export default function Login() {
             Sign In
           </button>
           <span>
-            New to Netflix? <b>Sign up now.</b>
+            New to Netflix?
+            <Link to="/register" className="link">
+              <b>Sign up now.</b>
+            </Link>
           </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you're not a
